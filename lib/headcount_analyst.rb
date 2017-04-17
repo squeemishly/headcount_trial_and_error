c39d128f41c069d7462075a00e24debd62eae7f3
squeemishly <squeemishly@gmail.com>
2017-04-17 07:02:14 -0600

    found averages for each district in headcount analyst

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -2,6 +2,24 @@ require_relative 'district_repository'
 
 
 class HeadcountAnalyst
+  attr_reader :dr
+
   def initialize(dr)
+    @dr = dr
+  end
+
+  def kindergarten_participation_rate_variation(district_1, district_2)
+    binding.pry
+
+  end
+
+  def district_average(district)
+    count, total = 0
+    stats = dr.find_by_name(district).enrollment.kindergarten_participation_by_year
+    stats.each do |key, value|
+      count += 1
+      total = total + value
+    end
+    average = (total/count).round(3)
   end
 end
--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -7,10 +7,35 @@ class HeadcountAnalystTest < Minitest::Test
 
   def setup
     @dr = DistrictRepository.new
+    @dr.load_data({
+      :enrollment => {
+        :kindergarten => "./test/fixtures/Kindergartners_in_a_full_day_program.csv"
+      }
+    })
     @ha = HeadcountAnalyst.new(dr)
   end
 
   def test_it_exists
     assert_instance_of HeadcountAnalyst, ha
+  end
+
+  def test_it_has_access_to_the_district_repository
+    assert_instance_of DistrictRepository, dr
+    assert_instance_of District, dr.find_by_name("ACADEMY 20")
+  end
+
+  def test_it_can_find_kindergarten_stats
+    skip
+    district = dr.find_by_name("ACADEMY 20")
+    assert_equal 0.436, district.enrollment.kindergarten_participation_in_year(2010)
+  end
+
+  def test_it_can_find_a_districts_average_participation
+    assert_equal 0.285, ha.district_average("ACADEMY 20")
+  end
+
+  def test_it_compares_a_districts_average_participation_against_another_district
+    skip
+    assert_equal 0.766, ha.kindergarten_participation_rate_variation('ACADEMY 20', :against => 'COLORADO')
   end
 end
